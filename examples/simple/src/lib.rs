47ba094a6cc93b151f4776239e008de3341a7110
Arturo Beccar-Varela <107512933+arturoBeccar@users.noreply.github.com>
2024-10-31 13:22:31 -0300

    Scale fee

--- examples/simple/src/lib.rs
+++ examples/simple/src/lib.rs
@@ -26,7 +26,7 @@ pub enum DataKey {
 pub struct FlashLoanReceiverContract;
 
 fn compute_fee(amount: &i128) -> i128 {
-    amount / 1250 // 0.05%, still TBD
+    100 * amount / 1250 // 0.05%, still TBD
 }
 
 #[contractimpl]
